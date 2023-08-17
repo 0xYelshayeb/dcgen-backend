4afba743cdfe4fc37c203d06f09a4c05527317c8
Yassine <ge87won@mytum.de>
2023-08-17 15:50:19 +0200

    Changed default port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Using CORS middleware
 app.use(cors());
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;;
 
 app.get('/timeSeries', (req, res) => {
     console.log("request received")
